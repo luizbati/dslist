b1e3d0be3950308487d84026849e3165795bce6a
Luiz <luizcb1944@hotmail.com>
2023-07-13 19:36:44 -0300

    Game repository.service.controller

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDto.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDto {
+	
+	
+	private long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+	
+
+	public GameMinDto () {
+		
+	}
+
+
+	public GameMinDto(Game entity) {
+		super();
+		id = entity.getId();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public Integer getYear() {
+		return year;
+	}
+
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+}
